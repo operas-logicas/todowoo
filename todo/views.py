9f2c72bdb4848bee57634a4404d5802406853aa8
Robert Miller <miller.robert.john@gmail.com>
2021-08-12 17:44:32 -0600

    Signupuser view and template

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.db import IntegrityError
+from django.shortcuts import redirect, render
 
 # Create your views here.
+def currenttodos(request):
+  return render(
+    request,
+    'todo/current.html'
+  )
+
+def signupuser(request):
+  if request.method == 'POST':
+    # Validate password
+    if request.POST['password1'] != request.POST['password2']:
+      error = 'Password confirmation must match password!'
+
+      return render(
+        request,
+        'todo/signup.html',
+        { 'form': UserCreationForm(), 'error': error }
+      )
+
+    # Try creating new user
+    else:
+      try:
+        user = User.objects.create_user(
+          request.POST['username'],
+          request.POST['password1']
+        )
+
+        # Save & login new user
+        user.save()
+        login(request, user)
+
+        # Redirect to currenttodos/
+        return redirect('currenttodos')
+
+      except IntegrityError:
+        error = 'Username already taken! Please choose a new username.'
+
+        return render(
+          request,
+          'todo/signup.html',
+          { 'form': UserCreationForm(), 'error': error }
+        )
+
+  else:
+    # GET request so send sign up form
+    return render(
+      request,
+      'todo/signup.html',
+      { 'form': UserCreationForm() }
+    )
--- todowoo/urls.py
+++ todowoo/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views
 
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
+
+    # Auth
+    path('signup/', views.signupuser, name='signupuser'),
+
+    # Todos
+    path('current/', views.currenttodos, name='currenttodos'),
 ]
